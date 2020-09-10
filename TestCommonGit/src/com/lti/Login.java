6331a678aca069edc78d14807a402205443a3d53
Hell-Spider <jeetonwheelstwice@gmail.com>
2020-09-10 17:23:34 +0530

    Statements added to both function

--- TestCommonGit/src/com/lti/Login.java
+++ TestCommonGit/src/com/lti/Login.java
@@ -5,13 +5,12 @@ public class Login
 	//registration method
 	public void registration()
 	{
-	
+		System.out.println("Registration method called");
 	}
 
 	//login method
 	public void login()
-
 	{
-		
+		System.out.println("Login method called");
 	}
 }
